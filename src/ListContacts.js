34880187ec4ea1e6544e3d7a58cebe32f4c99d2b
Mohamed Mostafa <mohamedhadia611@gmail.com>
2021-12-07 13:07:02 +0200

    Hook up remove button to remove contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function ListContacts(props) {
   return (
     <div>
-       <ol className="contact-list">
+      <ol className="contact-list">
         {props.contacts.map((contact) => (
           <li className="contact-list-item" key={contact.id}>
             <div
               className="contact-avatar"
               style={{ backgroundImage: `url(${contact.avatarURL})` }}
-            ></div>
+            />
             <div className="contact-details">
               <p>{contact.name}</p>
               <p>{contact.handle}</p>
             </div>
+            <button
+              onClick={() => props.onDeleteContact(contact)}
+              classNam="contact-remove"
+            >
+              Remove
+            </button>
           </li>
         ))}
       </ol>
     </div>
-  )
+  );
 }
