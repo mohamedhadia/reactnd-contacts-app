ab765d1f8c56d1199ddac1218c425f3da0717933
Mohamed Mostafa <mohamedhadia611@gmail.com>
2021-12-16 12:30:43 +0200

    Use useEffect to fetch contacts from the server

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ListContacts from "./ListContacts";
+import * as ContactsApi from "./utils/ContactsAPI";
+function App() {
+  const [contacts, setcontacts] = useState([]);
 
-class App extends Component {
-  state = {
-    contacts: [
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "@tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg",
-      },
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "@karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg",
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "@richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg",
-      },
-    ],
+  const removeContact = (contact) => {
+    setcontacts((prev) => prev.filter((c) => c.id !== contact.id));
+
+    // this.setState((currentState) => ({
+    //   contacts: currentState.contacts.filter((c) => {
+    //     return c.id !== contact.id;
+    //   }),
+    // }));
   };
 
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id;
-      }),
-    }));
-  };
-  render() {
-    return (
-      <div>
-        <ListContacts
-          onDeleteContact={this.removeContact}
-          contacts={this.state.contacts}
-        />
-      </div>
-    );
-  }
+  useEffect(() => {
+    ContactsApi.getAll().then((data) => {
+      setcontacts(data);
+    });
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <ListContacts onDeleteContact={removeContact} contacts={contacts} />
+    </div>
+  );
 }
 
 export default App;
